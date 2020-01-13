ad1cc5c291f58d58f97db4ea7d5b748a84e12bf5
Xwilarg <xwilarg@yahoo.fr>
2020-01-13 19:21:13 +0900

    Add shop elements

---
+++ Assets/Scripts/UI/ShopElement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopElement : MonoBehaviour
+{
+    [SerializeField]
+    private Text title, description, price;
+
+    [SerializeField]
+    private Button buyButton;
+
+    public void Init(Equipement eq)
+    {
+        title.text = eq.name;
+        description.text = eq.description;
+        price.text = "Price: " + eq.price;
+    }
+}
